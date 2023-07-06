3ab24a7b6bcd8c1efa8291518a1d0853d567c2c9
bashayer770 <beshow95@gmail.com>
2023-07-06 18:06:00 +0300

    frontEndDone

--- src/api/auth.js
+++ src/api/auth.js
@@ -12,8 +12,38 @@ const login = async (userInfo) => {
   }
 };
 
+const register = async (userInfo) => {
+  try {
+    // const formData = new FormData();
+    // for (const key in userInfo) formData.append(key, userInfo[key]);
+    const { data } = await instance.post("/auth/register", userInfo);
+    storeToken(data.token);
+    return data;
+  } catch (error) {
+    console.log(error.response);
+  }
+};
+
 const storeToken = (token) => {
   localStorage.setItem("token", token);
 };
 
-export { login };
+const checkToken = () => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    const decode = jwt_decode(token);
+    const currentTime = Date.now() / 1000;
+    if (decode.exp < currentTime) {
+      localStorage.removeItem("token");
+      return false;
+    }
+    return true;
+  }
+  return false;
+};
+
+const logout = () => {
+  localStorage.removeItem("token");
+};
+
+export { login, register, checkToken, logout };
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,10 +1,35 @@
 import React, { useContext, useState } from "react";
 import UserContext from "../context/UserContext";
+
 import { useNavigate } from "react-router-dom";
 import { isError, useMutation } from "react-query";
-import { register } from "../api/auth";
+import { checkToken, register } from "../api/auth";
+import { Navigate } from "react-router-dom/dist";
 
 const Register = () => {
+  const [user, setUser] = useContext(UserContext);
+  const [userInfo, setUserInfo] = useState({});
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setUserInfo({ ...userInfo, [e.target.name]: e.target.value });
+  };
+
+  const { mutate: registerFn } = useMutation({
+    mutationFn: () => register(userInfo),
+    onSuccess: () => setUser(checkToken()),
+  });
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    registerFn();
+    console.log(userInfo);
+    // navigate("/home");
+  };
+
+  if (user) {
+    // return <Navigate to="/home" />;
+  }
+
   return (
     <div className="bg-gray-100 ">
       <div className="bg-form-img pt-20 xl:pt-36 text-white h-[95vh] w-auto justify-center mx-auto ">
@@ -13,42 +38,48 @@ const Register = () => {
             <h2 className="card-title text-3xl justify-center text-center my-2">
               Register
             </h2>
-
-            <label>Username</label>
-            <input
-              name="username"
-              type="text"
-              placeholder="Username"
-              className="input opacity-70 w-full max-w-xs"
-            />
-            <label>Email</label>
-            <input
-              name="email"
-              type="email"
-              placeholder="Email"
-              className="input opacity-70 w-full max-w-xs"
-            />
-            <label>Password</label>
-            <input
-              name="password"
-              type="password"
-              placeholder="Password"
-              className="input opacity-70 w-full max-w-xs"
-            />
-            <label>Confirm Password</label>
-            <input
-              type="password"
-              placeholder="Confirm Password"
-              className="input opacity-70 w-full max-w-xs"
-            />
-            <div className="card-actions justify-center">
-              <button
-                type="submit"
-                className="opacity-90 px-4 py-2 mr-2 rounded bg-orange-500 hover:bg-orange-600 text-white mt-6"
-              >
-                Register
-              </button>
-            </div>
+            <form onSubmit={handleFormSubmit}>
+              <label>Username</label>
+              <input
+                name="username"
+                type="text"
+                onChange={handleChange}
+                placeholder="Username"
+                className="input opacity-70 w-full max-w-xs"
+              />
+              <label>Email</label>
+              <input
+                name="email"
+                type="email"
+                onChange={handleChange}
+                placeholder="Email"
+                className="input opacity-70 w-full max-w-xs"
+              />
+              <label>Password</label>
+              <input
+                name="password"
+                type="password"
+                onChange={handleChange}
+                placeholder="Password"
+                className="input opacity-70 w-full max-w-xs"
+              />
+              <label>Confirm Password</label>
+              <input
+                name="confirmPass"
+                type="password"
+                placeholder="Confirm Password"
+                onChange={handleChange}
+                className="input opacity-70 w-full max-w-xs"
+              />
+              <div className="card-actions justify-center">
+                <button
+                  type="submit"
+                  className="opacity-90 px-4 py-2 mr-2 rounded bg-orange-500 hover:bg-orange-600 text-white mt-6"
+                >
+                  Register
+                </button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
