af5e26a02be843042a1325591980739f84cddc26
bashayer770 <beshow95@gmail.com>
2023-07-11 16:55:57 +0300

    combobox add

--- src/component/ComboBox.js
+++ src/component/ComboBox.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { MultiSelect } from "react-multi-select-component";
 
 const ComboBox = () => {
@@ -10,19 +10,31 @@ const ComboBox = () => {
     { label: "Ingredient 3", value: "ingredient3" },
     // Add more options as needed
   ];
+  const onCreate = (value) => {
+    console.log(value);
 
+    return { label: value, value };
+  };
   const handleIngredientChange = (selected) => {
     setSelectedIngredients(selected);
   };
 
+  console.log(selectedIngredients);
+
   return (
     <div>
       <MultiSelect
+        onCreateOption={onCreate}
+        isCreatable={true}
         options={ingredientOptions}
         value={selectedIngredients}
         onChange={handleIngredientChange}
         labelledBy="Select ingredients"
       />
+      <button
+        className="btn btn-lg hover:shadow-lg transform hover:scale-105 transition duration-300 ease-in-out"
+        onClick={() => window.my_modal_5.showModal()}
+      ></button>
     </div>
   );
 };
